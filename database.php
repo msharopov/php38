fa5bc5ea7f569377ff5d9056d76e8a208f63b242
Mamurjon <msharopov@mail.rumsharopov@mail.ru>
2024-03-12 17:08:44 +0500

    php38

---
+++ database.php
@@ -0,0 +1,18 @@
+ <?php
+$servername = "localhost";
+$username = "meniki";
+$password = "12345";
+$database = "php-blog";
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+?> 
+
+
+
+
---
+++ post.php
@@ -0,0 +1,11 @@
+
+<?php 
+$title = 'Postni ko\'rish';
+require 'includes/header.php' 
+?>
+
+
+
+
+<?php require 'includes/footer.php' ?>;
+
